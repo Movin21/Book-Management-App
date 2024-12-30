3947bce827b02813d3b6f8209fdbb5a872c4c663
Movin21 <movinliyanage@gmail.com>
2024-12-30 23:30:26 +0530

    middleware for route protection

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,1 +1,16 @@
-import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button } from "@/components/ui/button";
+
+export default function LoginPage() {
+  return (
+    <main className="h-dvh flex-col items-center gap-6 text-4xl p-4">
+      <h1>Book Manager App</h1>
+      <Button asChild>
+        <LoginLink>Sign in</LoginLink>
+      </Button>
+      <Button asChild>
+        <LogoutLink>Log out</LogoutLink>
+      </Button>
+    </main>
+  );
+}
